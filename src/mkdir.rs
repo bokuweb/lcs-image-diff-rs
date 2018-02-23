2cc4e8f395aaf952b88021bf895810ccfe21fba7
bokuweb <bokuweb12@gmail.com>
2018-02-23 18:45:15 +0900

    fix: fixed a bug, panic occurred when dir is already exists

--- src/mkdir.rs
+++ src/mkdir.rs
@@ -5,8 +5,9 @@ use std::fs::create_dir_all;
 pub fn mkdirp<P: AsRef<Path>>(p: P) -> io::Result<()> {
     if let Err(e) = create_dir_all(p) {
         if e.kind() != io::ErrorKind::AlreadyExists {
-            return Err(e);
+            return Ok(());
         }
+        return Err(e);
     }
     Ok(())
 }
