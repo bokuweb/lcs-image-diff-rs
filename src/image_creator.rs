8aa8761ba308159e824f5c883e33d2a675abdbfd
bokuweb <bokuweb12@gmail.com>
2018-03-15 10:28:37 +0900

    fix: fixed a bug panic occurred when difference width image input

--- src/image_creator.rs
+++ src/image_creator.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::path::Path;
+use std::cmp;
 use super::image::*;
 use super::lcs_diff::*;
 use super::mkdir::*;
@@ -35,7 +36,8 @@ fn compute_range(r: &Vec<usize>) -> Vec<(usize, usize)> {
 }
 
 fn blend_diff_area<G>(img: &mut G, ranges: Vec<(usize, usize)>, rgb: (u8, u8, u8), rate: f32)
-    where G: GenericImage<Pixel = Rgba<u8>>
+where
+    G: GenericImage<Pixel = Rgba<u8>>,
 {
     for (y1, y2) in ranges {
         for y in y1..(y2 + 1) {
@@ -50,32 +52,44 @@ fn blend_diff_area<G>(img: &mut G, ranges: Vec<(usize, usize)>, rgb: (u8, u8, u8
 
 fn blend(base: Rgba<u8>, rgb: (u8, u8, u8), rate: f32) -> Rgba<u8> {
     return Rgba {
-               data: [(base.data[0] as f32 * (1.0 - rate) + rgb.0 as f32 * (rate)) as u8,
-                      (base.data[1] as f32 * (1.0 - rate) + rgb.1 as f32 * (rate)) as u8,
-                      (base.data[2] as f32 * (1.0 - rate) + rgb.2 as f32 * (rate)) as u8,
-                      base.data[3]],
-           };
+        data: [
+            (base.data[0] as f32 * (1.0 - rate) + rgb.0 as f32 * (rate)) as u8,
+            (base.data[1] as f32 * (1.0 - rate) + rgb.1 as f32 * (rate)) as u8,
+            (base.data[2] as f32 * (1.0 - rate) + rgb.2 as f32 * (rate)) as u8,
+            base.data[3],
+        ],
+    };
 }
 
-fn put_diff_pixels(y: usize,
-                   img: &mut ImageBuffer<Rgba<u8>, Vec<u8>>,
-                   data: &String,
-                   rgb: (u8, u8, u8),
-                   rate: f32) {
+fn put_diff_pixels(
+    y: usize,
+    img: &mut ImageBuffer<Rgba<u8>, Vec<u8>>,
+    row_width: u32,
+    data: &String,
+    rgb: (u8, u8, u8),
+    rate: f32,
+) {
     let row = decode(data).unwrap();
     for x in 0..img.dimensions().0 {
         let index = x as usize * 4;
-        let pixel: Rgba<u8> =
-            Rgba { data: [row[index], row[index + 1], row[index + 2], row[index + 3]] };
+        let pixel: Rgba<u8> = if row_width > x {
+            Rgba {
+                data: [row[index], row[index + 1], row[index + 2], row[index + 3]],
+            }
+        } else {
+            Rgba { data: [0, 0, 0, 0] }
+        };
         img.put_pixel(x as u32, y as u32, blend(pixel, rgb, rate));
     }
 }
 
-pub fn save_marked_org_image(filename: &str,
-                             base: &mut DynamicImage,
-                             color: (u8, u8, u8),
-                             rate: f32,
-                             indexes: &Vec<usize>) {
+pub fn save_marked_org_image(
+    filename: &str,
+    base: &mut DynamicImage,
+    color: (u8, u8, u8),
+    rate: f32,
+    indexes: &Vec<usize>,
+) {
     let range = compute_range(indexes);
     let path = Path::new(filename).parent().unwrap();
     let _result = mkdirp(path);
@@ -84,18 +98,29 @@ pub fn save_marked_org_image(filename: &str,
     base.save(fout, PNG).unwrap();
 }
 
-pub fn save_diff_image(filename: &str, width: u32, result: &Vec<DiffResult<String>>, rate: f32) {
+pub fn save_diff_image(
+    filename: &str,
+    before_width: u32,
+    after_width: u32,
+    result: &Vec<DiffResult<String>>,
+    rate: f32,
+) {
     let path = Path::new(filename).parent().unwrap();
     let _result = mkdirp(path);
     let height = result.len() as u32;
+    let width = cmp::max(before_width, after_width);
     let mut img: ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::new(width, height);
     for (y, d) in result.iter().enumerate() {
         match d {
-            &DiffResult::Added(ref a) => put_diff_pixels(y, &mut img, &a.data, (99, 195, 99), rate),
-            &DiffResult::Removed(ref r) => {
-                put_diff_pixels(y, &mut img, &r.data, (255, 119, 119), rate)
+            &DiffResult::Added(ref a) => {
+                put_diff_pixels(y, &mut img, after_width, &a.data, (99, 195, 99), rate)
             }
-            &DiffResult::Common(ref c) => put_diff_pixels(y, &mut img, &c.data, (0, 0, 0), 0.0),
+            &DiffResult::Removed(ref r) => {
+                put_diff_pixels(y, &mut img, before_width, &r.data, (255, 119, 119), rate)
+            }
+            &DiffResult::Common(ref c) => {
+                put_diff_pixels(y, &mut img, width, &c.data, (0, 0, 0), 0.0)
+            }
         }
     }
     let ref mut fout = File::create(filename).unwrap();
